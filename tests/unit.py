95d6dbc75ec90ea253af2f62e5961d52559ccf24
LukeMurphey <Luke.Murphey@gmail.com>
2013-04-09 01:28:55 -0500

    Added unit tests

--- tests/unit.py
+++ tests/unit.py
@@ -1,6 +1,9 @@
 import unittest
 import sys
 import os
+import time
+import shutil
+import tempfile
 from StringIO import StringIO
 
 sys.path.append( os.path.join("..", "src", "bin") )
@@ -45,6 +48,16 @@ class TestDurationField(unittest.TestCase):
     
 class TestWebPing(unittest.TestCase):
     
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp( prefix="TestWebPing" )
+        #os.makedirs(self.tmp_dir)
+        
+    def tearDown(self):
+        shutil.rmtree( self.tmp_dir )
+        
+    def test_get_file_path(self):
+        self.assertEquals( WebPing.get_file_path( "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_ping", "web_ping://TextCritical.com"), "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_ping/35163af7282b92013f810b2b4822d7df.json")
+    
     def test_ping(self):
         
         url_field = URLField( "test_ping", "title", "this is a test" )
@@ -59,12 +72,37 @@ class TestWebPing(unittest.TestCase):
     def test_ping_timeout(self):
         url_field = URLField( "test_ping_timeout", "title", "this is a test" )
         
-        result = WebPing.ping( url_field.to_python("https://10.0.23.23/"), timeout=3 )
+        result = WebPing.ping( url_field.to_python("https://192.168.30.23/"), timeout=3 )
         
         self.assertEquals(result.timed_out, True)
         
+    def test_save_checkpoint(self):
+        WebPing.save_checkpoint(self.tmp_dir, "web_ping://TextCritical.com", 100)
+        self.assertEquals( WebPing.last_ran(self.tmp_dir, "web_ping://TextCritical.com"), 100)
+        
+    def test_is_expired(self):
+        self.assertFalse( WebPing.is_expired(time.time(), 30) )
+        self.assertTrue( WebPing.is_expired(time.time() - 31, 30) )
+        
+    def get_test_dir(self):
+        return os.path.dirname(os.path.abspath(__file__))
+        
+    def test_needs_another_run(self):
+        
+        # Test case where file does not exist
+        self.assertTrue( WebPing.needs_another_run( "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_ping", "web_ping://DoesNotExist", 60 ) )
+        
+        # Test an interval right at the earlier edge
+        self.assertFalse( WebPing.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_ping://TextCritical.com", 60, 1365486765 ) )
+        
+        # Test an interval at the later edge
+        self.assertFalse( WebPing.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_ping://TextCritical.com", 10, 1365486775 ) )
+        
+        # Test interval beyond later edge
+        self.assertTrue( WebPing.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_ping://TextCritical.com", 10, 1365486776 ) )
+        
     def test_send_result(self):
-        web_ping = WebPing()
+        web_ping = WebPing(timeout=3)
         
         url_field = URLField( "test_ping", "title", "this is a test" )
         result = WebPing.ping( url_field.to_python("https://www.google.com/") )
@@ -73,4 +111,16 @@ class TestWebPing(unittest.TestCase):
         
         web_ping.send_result(result, "stanza", "title", unbroken=True, close=True, out=out)
         
-        self.assertEqual(out.getvalue(), "")
+        self.assertTrue(out.getvalue().find("response_code=200") >= 0)
+        
+    def test_send_result_unavailable(self):
+        web_ping = WebPing(timeout=3)
+        
+        url_field = URLField( "test_ping", "title", "this is a test" )
+        result = WebPing.ping( url_field.to_python("http://192.168.30.23/"), timeout=3 )
+        
+        out = StringIO()
+        
+        web_ping.send_result(result, "stanza", "title", unbroken=True, close=True, out=out)
+        
+        self.assertTrue(out.getvalue().find("timed_out=True") >= 0)
