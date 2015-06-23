8c7c547c7021ef139d54e47124130f3eba3d4880
Luke Murphey <Luke@LukeMurphey.net>
2015-06-22 20:09:26 -0500

    Adding an information message that only shows up when no inputs exist

--- src/appserver/static/status_overview.js
+++ src/appserver/static/status_overview.js
@@ -1,17 +1,39 @@
 require.config({
     paths: {
-    	website_status_cell_renderer: '../app/website_monitoring/WebsiteStatusCellRenderer'
+    	website_status_cell_renderer: '../app/website_monitoring/WebsiteStatusCellRenderer',
+    	info_message_view: '../app/insteon/js/views/InfoMessageView'
     }
 });
 
-require(['jquery','underscore','splunkjs/mvc', 'website_status_cell_renderer', 'splunkjs/mvc/simplexml/ready!'],
-	function($, _, mvc, WebsiteStatusCellRenderer){
+require(['jquery','underscore','splunkjs/mvc', 'website_status_cell_renderer', 'info_message_view', 'splunkjs/mvc/searchmanager', 'splunkjs/mvc/simplexml/ready!'],
+	function($, _, mvc, WebsiteStatusCellRenderer, InfoMessageView, SearchManager){
 	
-	    var statusTable = mvc.Components.get('element2');
+		// Setup the cell renderer
+	    var statusTable = mvc.Components.get('element1');
 	
 	    statusTable.getVisualization(function(tableView){
 	        tableView.table.addCellRenderer(new WebsiteStatusCellRenderer());
 	        tableView.table.render();
+	    });
+	    
+
+	    // Make the search that will determine if website monitoring inputs exist
+	    var hasInputSearch = new SearchManager({
+	        "id": "webping-inputs-search",
+	        "earliest_time": "-24h@h",
+	        "latest_time": "now",
+	        "search":'| rest /services/data/inputs/web_ping | stats count',
+	        "cancelOnUnload": true,
+	        "autostart": false,
+	        "app": '',
+	        "auto_cancel": 90,
+	        "preview": false
+	    }, {tokens: false});
+	
+	    var infoMessageView = new InfoMessageView({
+	    	search_manager: hasInputSearch,
+	    	message: 'No website monitoring inputs exist yet; create one to monitor a website. <a target="_blank" href="../../manager/website_monitoring/adddata/selectsource?input_type=web_ping&modinput=1&input_mode=1">Create a website monitoring input now.</a>',
+	    	eval_function: function(searchResults){ return searchResults.rows[0][0] === "0" }
 	    });
 	    
 	}
