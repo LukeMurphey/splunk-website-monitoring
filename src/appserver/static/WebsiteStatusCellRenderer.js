24935ece65834c02197bb713ada7ca7fb5712f32
lmurphey <lmurphey@192.168.34.211>
2016-10-28 02:48:05 -0500

    Adding stopwatch icons to the renderer

--- src/appserver/static/WebsiteStatusCellRenderer.js
+++ src/appserver/static/WebsiteStatusCellRenderer.js
@@ -10,7 +10,7 @@ define(['jquery', 'underscore', 'splunkjs/mvc', 'views/shared/results_table/rend
 			 // Add the class so that the CSS can style the content
 			 $td.addClass(cell.field);
 			 
-			 var icon = "";
+			 var icon = null;
 			 
 			 // Handle the response_code
 			 if( cell.field == "response_code" ){
@@ -33,15 +33,44 @@ define(['jquery', 'underscore', 'splunkjs/mvc', 'views/shared/results_table/rend
 			 }
 			 
 			 // Handle the response_time and average fields
-			 else if( cell.field == "response_time" ||  cell.field == "average" ){
+			 else if( cell.field == "response_time" || cell.field == "average" ){
 				 
 				 var float_value = parseFloat(cell.value, 10);
 				 
 				 if( float_value >= 1000 ){
 					 $td.addClass("failure");
+					 icon = 'alert';
 				 }
 				 else{
 					 $td.addClass("success");
+					 
+					 var percent = 0;
+					 
+					 if(float_value <= 250){
+						 percent = 25;
+					 }
+					 else if(float_value <= 500){
+						 percent = 50;
+					 }
+					 else if(float_value <= 750){
+						 percent = 75;
+					 }
+					 else if(float_value <= 1000){
+						 percent = 100;
+					 }
+					 else{
+						 percent = null;
+					 }
+					 
+					 if( percent !== null){
+					     $td.html(_.template('<img class="performance_icon" height="12" width="12" src="/static/app/website_monitoring/img/<%- percent %>.png" /> <%- value %>', {
+				            	value: cell.value,
+				            	percent: percent
+				         }));
+					     
+					     return;
+					 }
+
 				 }
 				 
 			 }
