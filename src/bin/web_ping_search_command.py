f416f932eb944fd87ac5b19172ec5bb10b7b0165
Luke Murphey <Luke.Murphey@gmail.com>
2019-11-27 13:19:10 -0800

    Adding headers option to the search command

--- src/bin/web_ping_search_command.py
+++ src/bin/web_ping_search_command.py
@@ -16,11 +16,12 @@ class WebPingSearchCommand(SearchCommand):
     This search command provides a Splunk interface for doing a web-ping.
     """
 
-    def __init__(self, url=None, expected_string=None):
+    def __init__(self, url=None, expected_string=None, return_headers=False):
         SearchCommand.__init__(self, run_in_preview=False, logger_name="webping_search_command")
 
         self.url = url
         self.expected_string = expected_string
+        self.return_headers = return_headers
 
         self.logger.info("Web-ping running against url=%s", url)
 
@@ -38,7 +39,7 @@ class WebPingSearchCommand(SearchCommand):
         url_parsed = url_field.to_python(self.url)
 
         # Do the web-ping
-        result = WebPing.ping(url_parsed, logger=self.logger, should_contain_string=self.expected_string)
+        result = WebPing.ping(url_parsed, logger=self.logger, should_contain_string=self.expected_string, return_headers=self.return_headers)
 
         # Prep the result dictionary
         data = {
@@ -48,21 +49,26 @@ class WebPingSearchCommand(SearchCommand):
             'url': result.url
         }
 
-        # Add the MD5 of the response of available
+        # Add the MD5 of the response if available
         if result.response_md5 is not None:
             data['content_md5'] = result.response_md5
 
-        # Add the SHA-224 of the response of available
+        # Add the SHA-224 of the response if available
         if result.response_sha224 is not None:
             data['content_sha224'] = result.response_sha224
 
-        # Add the MD5 of the response of available
+        # Add the size of the response if available
         if result.response_size is not None:
             data['content_size'] = result.response_size
 
         # Add the variable noting if the expected string was present
         if result.has_expected_string is not None:
             data['has_expected_string'] = str(result.has_expected_string).lower()
+
+        # Add the the headers if present
+        if result.headers is not None:
+            for header in result.headers:
+               data['header_' + header] = result.headers[header]
 
         # Output the results
         self.output_results([data])
