9beefb715e04566def59d8f05199908027ad13c0
Luke Murphey <Luke.Murphey@gmail.com>
2022-05-03 15:16:29 -0700

    Making proxy server retry when it cannot start

--- tests/test_proxy_server.py
+++ tests/test_proxy_server.py
@@ -4,7 +4,10 @@ This module serves a basic HTTP proxy which just passes on HTTP requests.
 from six.moves.socketserver import TCPServer
 from six.moves.urllib.request import urlopen
 from six.moves.SimpleHTTPServer import SimpleHTTPRequestHandler
+import time
 
+WAITING_FOR_PORT_SLEEP_TIME = 4
+WAITING_FOR_PORT_ATTEMPT_LIMIT = 75
 class Proxy(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -15,6 +18,16 @@ def get_server(port):
     """
     Call proxyd.shutdown() to stop the server
     """
-    
-    proxyd = TCPServer(("", port), Proxy)
-    return proxyd
+    attempts = 0
+    while attempts < WAITING_FOR_PORT_ATTEMPT_LIMIT:
+        try:
+            proxyd = TCPServer(("", port), Proxy)
+            return proxyd
+                
+        except IOError:
+            time.sleep(WAITING_FOR_PORT_SLEEP_TIME)
+            attempts = attempts + 1
+        
+    raise IOError("Could not start proxy server")
+
+
